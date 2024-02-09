75dcfae54592737445f370456eee8f908b293ba8
Varga Petra <90456824+vapabie@users.noreply.github.com>
2024-02-09 17:17:22 +0100

    Add backend

---
+++ Backend/src/test/java/com/example/calender/CalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
