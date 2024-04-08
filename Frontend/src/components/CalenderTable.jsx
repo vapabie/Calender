f834ac44059569269571c51709cef57d9290e8e1
Varga Petra <90456824+vapabie@users.noreply.github.com>
2024-04-08 12:47:09 +0200

    Create Calender table component

---
+++ Frontend/src/components/CalenderTable.jsx
@@ -0,0 +1,19 @@
+export default function CalenderTable(){
+    return(
+
+<table className='Calender-table'>
+                <thead>
+                <tr>
+                    <td>mon</td>
+                    <td>tru</td>
+                    <td>wed</td>
+                    <td>thu</td>
+                    <td>fri</td>
+                    <td>sat</td>
+                    <td>sun</td>
+                  </tr>
+                </thead>
+              </table>
+    );
+
+}
