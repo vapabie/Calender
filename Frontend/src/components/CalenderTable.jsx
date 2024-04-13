1a119aa0289abfc058eb1d66b4a1525ce8f5c9a4
Varga Petra <90456824+vapabie@users.noreply.github.com>
2024-04-13 20:26:44 +0200

    calender prev/next button, current month-year, and weekdays

--- Frontend/src/components/CalenderTable.jsx
+++ Frontend/src/components/CalenderTable.jsx
@@ -1,19 +1,39 @@
-export default function CalenderTable(){
-    return(
+function CalenderTable() {
+  const CurrentYear = 2012;
+  const CurrentMonth = "January";
 
-<table className='Calender-table'>
-                <thead>
-                <tr>
-                    <td>mon</td>
-                    <td>tru</td>
-                    <td>wed</td>
-                    <td>thu</td>
-                    <td>fri</td>
-                    <td>sat</td>
-                    <td>sun</td>
-                  </tr>
-                </thead>
-              </table>
-    );
-
+  return (
+    <div className="calTab">
+      <table className="cbar">
+        <tbody>
+          <tr>
+            <td className="prevMonthTD">
+              <button className=" btn">&lt;</button>
+            </td>
+            <td className="monthNow">
+              {CurrentMonth} {CurrentYear}
+            </td>
+            <td className="nextMonthTD">
+              <button className="btn">&gt;</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="weekdays">
+        <thead>
+          <tr>
+            <th>M</th>
+            <th>Tu</th>
+            <th>W</th>
+            <th>Th</th>
+            <th>F</th>
+            <th>Sa</th>
+            <th>Su</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  );
 }
+export default CalenderTable;
