fd7f1771a5c038c1afb7cb2939c6d8fbe734a2b9
Varga Petra <90456824+vapabie@users.noreply.github.com>
2024-04-14 11:19:15 +0200

    update app jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react';
-import Appbar from './components/Appbar';
-import CalenderTable from './components/CalenderTable';
-//import Sidebar from './components/Sidebar';
-import './App.css';
+import NavBar from "./components/NavBar";
+import Login from "./components/Login";
+import CalenderTable from "./components/CalenderTable";
+import "./App.css";
+import Preview from "./components/Preview";
 
 function App() {
   return (
-        <div>
-          <div className="Appbar-container">
-            <Appbar/>
+    <div>
+      <NavBar />
+      <div className="Body-container">
+        <div className="LeftSideBar-container">
+          <div className="MiniCalender-container">
+            <CalenderTable />
           </div>
-
-          <div className="Body-container">
-            <div className='LeftSideBar-container'>
-              <div className='MiniCalender-container'>
-
-              </div>
-              <div className='Preview-container'>
-                
-              </div>
-            </div>
-            <div className='Center-container'>
-              <CalenderTable/>
-            </div>
-            <div className='RightSideBar-container'></div>
-            
+          <div className="Preview-container">
+            <Preview />
           </div>
         </div>
-       
-    
+        <div className="Center-container">
+          <CalenderTable />
+        </div>
+        <div className="RightSideBar-container"></div>
+      </div>
+    </div>
   );
 }
 
-export default App
+export default App;
