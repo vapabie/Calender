68e551e9028c4011e97841d320c8f49af73afd21
Varga Petra <90456824+vapabie@users.noreply.github.com>
2024-05-08 17:57:52 +0200

    Add prototype of adding an item as admin

---
+++ Frontend/src/components/AddItem.jsx
@@ -0,0 +1,17 @@
+function AddItem() {
+  return (
+    <div>
+      <div>
+        <label>name</label>
+        <input />
+      </div>
+      <div>
+        <label>reward</label>
+        <input />
+      </div>
+      <button>save</button>
+    </div>
+  );
+}
+
+export default AddItem;
