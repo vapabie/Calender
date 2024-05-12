7ff276540344f68b48a0bdcd28cc7063d4141df1
Varga Petra <90456824+vapabie@users.noreply.github.com>
2024-05-12 12:05:17 +0200

    Add ModelMapper

--- Backend/src/main/java/com/example/calender/CalenderApplication.java
+++ Backend/src/main/java/com/example/calender/CalenderApplication.java
@@ -1,13 +1,20 @@
 package com.example.calender;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CalenderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalenderApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getModelMapper(){
+		return new ModelMapper();
 	}
 
 }
