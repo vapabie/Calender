f515bc2de11fced8739baa640690d6453891898e
Varga Petra <90456824+vapabie@users.noreply.github.com>
2024-07-11 13:47:56 +0200

    insert  initial data into database

---
+++ Backend/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+Insert Into color (colorid, hexcode, colorname)
+values (1, '#b00b69', 'Dark Purple'),
+       (2, '#ABC123', 'Green'),
+       (3, '#3d90d9', 'Blue');
+
+Insert Into priority (priorityid, priorityname, prioritypoints)
+values (1,'busy', 10),
+       (2, 'free', 5),
+       (3, 'middle', 8);
