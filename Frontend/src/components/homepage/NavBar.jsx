e19e3d739c7af1208b39fe194a3d879ee007899e
Varga Petra <90456824+vapabie@users.noreply.github.com>
2024-09-14 15:26:55 +0200

    delete search bar from navbar

--- Frontend/src/components/homepage/NavBar.jsx
+++ Frontend/src/components/homepage/NavBar.jsx
@@ -41,17 +41,6 @@ export default function NavBar({ isAdmin, onAddClicked, onSwitchClicked }) {
           </div>
         ) : (
           <div className="container-fluid">
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
             <div className="btn-group">
               <a href="month" className="btn btn-primary">
                 Month
