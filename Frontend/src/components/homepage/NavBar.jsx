728353605444b1065c5323b6495567294cc0b59f
Varga Petra <90456824+vapabie@users.noreply.github.com>
2024-11-11 22:16:50 +0100

    update navigation bar

--- Frontend/src/components/homepage/NavBar.jsx
+++ Frontend/src/components/homepage/NavBar.jsx
@@ -1,4 +1,9 @@
-export default function NavBar({ isAdmin, onAddClicked, onSwitchClicked }) {
+export default function NavBar({
+  isAdmin,
+  onAddClicked,
+  onSwitchClicked,
+  name,
+}) {
   const handleAddClicked = () => {
     onAddClicked();
     console.log("clicked");
@@ -6,9 +11,9 @@ export default function NavBar({ isAdmin, onAddClicked, onSwitchClicked }) {
 
   return (
     <div>
-      <nav className="navbar bg-body-tertiary">
+      <nav className="nav">
         {isAdmin ? (
-          <div className="container-fluid">
+          <div className="container-fluid navcon">
             <div className="btn-group">
               <button
                 href="colors"
@@ -32,32 +37,45 @@ export default function NavBar({ isAdmin, onAddClicked, onSwitchClicked }) {
                 Priority
               </button>
             </div>
-            <button type="button" className="btn" onClick={handleAddClicked}>
-              +
-            </button>
-            <button type="button" className="btn">
-              p
-            </button>
+            <div className="navbuttons">
+              <button type="button" className="btn" onClick={handleAddClicked}>
+                +
+              </button>
+              <button type="button" className="btn">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-person"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                </svg>
+              </button>
+            </div>
           </div>
         ) : (
-          <div className="container-fluid">
+          <div className="container-fluid navcon">
             <div className="btn-group">
-              <a href="month" className="btn btn-primary">
-                Month
-              </a>
-              <a href="week" className="btn btn-primary">
-                Week
-              </a>
+              <a className="btn btn-primary">Month</a>
             </div>
+            <div>{name}'s Calendar</div>
             <div className="anp">
               <button type="button" className="btn" onClick={handleAddClicked}>
                 +
               </button>
               <button type="button" className="btn">
-                n
-              </button>
-              <button type="button" className="btn">
-                p
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-person"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                </svg>
               </button>
             </div>
           </div>
