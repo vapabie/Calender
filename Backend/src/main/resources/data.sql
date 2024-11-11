b112e7d4d71516ab74a82971de1a1da66de7b9c0
Varga Petra <90456824+vapabie@users.noreply.github.com>
2024-11-11 22:18:14 +0100

    base data for database

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -6,4 +6,14 @@ values (1, '#b00b69', 'Dark Purple'),
 Insert Into priority (priorityid, priorityname, prioritypoints)
 values (1,'busy', 10),
        (2, 'free', 5),
-       (3, 'middle', 8);
+       (3, 'flexible', 8);
+
+
+Insert Into rewards (rewardid, r_name, r_price)
+values (1, 'water', 7),
+       (2, 'headpat', 20),
+       (3, 'cake', 15);
+
+Insert Into userc (userid, email, userpassword, name, disadmin, points)
+values (1, 'admin@admin.com', 'admin', 'admin', true, 0),
+       (2, 'user@user.com', 'user', 'user', false, 35);
